8c0a7408b73c8658cbe96881fa29ed5a04b42ec7
James Black <=>
2010-09-14 15:01:39 +0800

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ ServerTracker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "companies#index"
 
   # See how all your routes lay out with "rake routes"
 
